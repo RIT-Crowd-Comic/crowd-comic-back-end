a0637d6182bb7f7efde8dcde7e32b520b65fbdb3
nca6109 <nca6109@g.rit.edu>
2024-07-12 11:12:04 -0500

    Update param name

--- src/services/sessionService.ts
+++ src/services/sessionService.ts
@@ -6,13 +6,13 @@ import { ISession } from '../models';
  * @param {string} _id ID of the user session is being created for
  * @returns ID of the session and ID of the user
  */
-const createSession = (sequelize: Sequelize) => async (_id: string) => {
+const createSession = (sequelize: Sequelize) => async (_user_id: string) => {
 
     // Delete any other session data for the user
-    await sequelize.models.session.destroy({ where: { id: _id } });
+    await sequelize.models.session.destroy({ where: { id: _user_id } });
 
     // Create new session for the user
-    const { id, user_id } = await sequelize.models.session.create({ user_id: _id, }) as ISession;
+    const { id, user_id } = await sequelize.models.session.create({ user_id: _user_id, }) as ISession;
 
     return { id, user_id };
 };

d95dbc94301f5c4d021a5eec7b4e05d9303e9ece
William Tallarico <wct9667@rit.edu>
2024-07-12 12:56:49 -0400

    linted

--- src/s3Init.ts
+++ src/s3Init.ts
@@ -1,53 +1,47 @@
 import  { S3Client, CreateBucketCommand  } from '@aws-sdk/client-s3';
 
 import dotenv from 'dotenv';
-const S3rver = require('s3rver');
 
+
+import s3rver from 's3rver';
 dotenv.config();
 
-const aws = new S3rver({
-    port: 5000,
-    hostname: 'localhost',
-    silent: false,
+const aws = new s3rver({
+    port:      5000,
+    silent:    false,
     directory: '/tmp/s3rver_test_directory'
 });
 
-aws.run((err : Error, hostname : string, port : number, directory : string) => {
-    if (err) {
-      console.error('Error starting server:', err);
-    } else {
-      console.log(`s3rver running at http://${hostname}:${port}, using directory: ${directory}`);
-    }
+aws.run(() => {
+    console.log('Server for aws running.');
 });
 
 const config = {
     forcePathStyle: true,
-    credentials: {
-      accessKeyId: 'S3RVER',
-      secretAccessKey:  'S3RVER',
+    credentials:    {
+        accessKeyId:     'S3RVER',
+        secretAccessKey: 'S3RVER',
     },
-    region: process.env.BUCKET_REGION as string,
+    region:   process.env.BUCKET_REGION as string,
     endpoint: 'http://localhost:5000'
-}
+};
 
-const s3 = new S3Client(
-    /*credentials: {
-        accessKeyId:     process.env.S3_ACCESS_KEY as string,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
-    },
-    region: process.env.BUCKET_REGION as string*/
-    config
-);
-
+/* credentials: {
+    accessKeyId:     process.env.S3_ACCESS_KEY as string,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
+},
+region: process.env.BUCKET_REGION as string*/
+const s3 = new S3Client(config);
 const bucketName = process.env.BUCKET_NAME;
 
 async function createBucket() {
     try {
         const createBucketCommand = new CreateBucketCommand({ Bucket: bucketName });
         const response = await s3.send(createBucketCommand);
-        console.log("Bucket created successfully:", response);
-    } catch (error) {
-        console.error("Error creating bucket:", error);
+        console.log('Bucket created successfully:', response);
+    }
+    catch (error) {
+        console.error('Error creating bucket:', error);
     }
 }
 

6e28943d76099256ec377f018efff5dcba955704
William Tallarico <wct9667@rit.edu>
2024-07-12 14:00:18 -0400

    additional error handling

--- src/s3Init.ts
+++ src/s3Init.ts
@@ -6,33 +6,44 @@ import dotenv from 'dotenv';
 import s3rver from 's3rver';
 dotenv.config();
 
-const aws = new s3rver({
-    port:      5000,
-    silent:    false,
-    directory: '/tmp/s3rver_test_directory'
-});
+try {
+    const aws = new s3rver({
+        port:      5000,
+        silent:    false,
+        directory: '/tmp/s3rver_test_directory'
+    });
+    
+    aws.run(() => {
+        console.log('Server for aws running.');
+    });
+}
+catch(error){ console.error('An error occurred during aws s3ver setup:', error);} 
 
-aws.run(() => {
-    console.log('Server for aws running.');
-});
-
-const config = {
-    forcePathStyle: true,
-    credentials:    {
-        accessKeyId:     'S3RVER',
-        secretAccessKey: 'S3RVER',
+let s3 : S3Client;
+let bucketName : string | undefined;
+try{
+    const config = {
+        forcePathStyle: true,
+        credentials:    {
+            accessKeyId:     'S3RVER',
+            secretAccessKey: 'S3RVER',
+        },
+        region:   process.env.BUCKET_REGION as string,
+        endpoint: 'http://localhost:5000'
+    };
+    
+    /* credentials: {
+        accessKeyId:     process.env.S3_ACCESS_KEY as string,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
     },
-    region:   process.env.BUCKET_REGION as string,
-    endpoint: 'http://localhost:5000'
+    region: process.env.BUCKET_REGION as string*/
+    s3 = new S3Client(config);
+    bucketName = process.env.BUCKET_NAME;
+}
+catch(error){
+    console.error('An error occurred s3 setup. Ensure that .env is setup properly and endpoint is correct.', error);
 };
 
-/* credentials: {
-    accessKeyId:     process.env.S3_ACCESS_KEY as string,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
-},
-region: process.env.BUCKET_REGION as string*/
-const s3 = new S3Client(config);
-const bucketName = process.env.BUCKET_NAME;
 
 async function createBucket() {
     try {
@@ -41,7 +52,7 @@ async function createBucket() {
         console.log('Bucket created successfully:', response);
     }
     catch (error) {
-        console.error('S3 Error: Error creating bucket:', error);
+        console.error('S3 Error: Error creating bucket. Ensure that .env is setup properly:', error);
     }
 }
 
